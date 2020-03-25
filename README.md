b1d2e0f20a2e9f005e665ce440230808c677e173
jwu150 <jwu15@hotmail.com>
2020-03-24 22:06:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-18n4ea
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-18n4ea)
---
+++ index.js
@@ -0,0 +1,40 @@
+// Import stylesheets
+import './style.css';
+
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = 
+`<div>
+<button id="startBtn" type="button">Start Timer</button>
+<div id="percentage">&nbsp;</div>
+<div id="outer" class="outer">
+  <div id="inner" class="inner"></div>
+  <div id="completion" class="completion"></div>
+</div>
+</div>`;
+
+const buttonRef = document.getElementById('startBtn');
+let intervalId = -1;
+let progress = 0;
+
+function getProgress() {
+  // const progress = Math.floor(Math.random() * Math.floor(100));
+  progress += 10;
+  return progress;
+}
+
+function startProgress() {
+  const percent = getProgress();
+  document.getElementById('completion').innerHTML = percent + '%';
+  document.getElementById('inner').style.width = percent + '%';
+  if (percent === 100) {
+    clearInterval(intervalId);
+    document.getElementById('completion').innerHTML = 'completed';
+  }
+}
+
+buttonRef.addEventListener("mousedown", (arg) => {
+  console.log();
+  intervalId = setInterval(startProgress, 500);
+});
+
+
